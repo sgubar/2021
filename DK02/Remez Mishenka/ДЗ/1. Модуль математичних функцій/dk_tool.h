cf0f17feca6961b2c5baaec6f40c8819baa83792
mad03day <mad03day@users.noreply.github.com>
2021-02-01 23:15:10 +0200

    ДЗ 1.Модуль математичних функцій

---
+++ DK02/Remez Mishenka/ДЗ/1. Модуль математичних функцій/dk_tool.h
@@ -0,0 +1,7 @@
+#include <stdio.h>		// Підключення бібліотек тут бо звик так робити :)
+#include <Windows.h>
+
+#ifdef DK_TOOL_H
+#define DK_TOOL_H
+	int limonchik(int* arr);		// Реально не запитуйте чого саме така назва
+#endif
---
+++ DK02/Remez Mishenka/ДЗ/1. Модуль математичних функцій/main.c
@@ -0,0 +1,22 @@
+﻿#include "dk_tool.h"        // Всі необхідні бібліотеки підключені
+
+#define MAX_NUMBER 4        // Розмір для задання масиву (на випадок якщо вам закортить попросити мене переробити код)
+
+char masive[MAX_NUMBER + 1], arr[MAX_NUMBER];
+
+
+int main(int argc, char* argv[])
+{
+    SetConsoleCP (1251);
+    SetConsoleOutputCP (1251);
+    printf ("Введите 8-миричное число: ");
+    fgets (masive, 5, stdin);    
+
+    printf("%d\n", limonchik(masive));      //Вибачте за назву, але нічого краще в 11 годин вечора в голову не прийшло
+
+    system("pause");        // Пишу у віжуалці
+    return 0;           // Повертаємо 0 як знак об успішності функції
+}
+
+// Оскільки пишу код у віжуалці іноді можете побачити scanf_s(); замість scanf();
+
