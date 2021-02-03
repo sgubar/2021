5775174e8f71615dfba70253d3b14d520990c297
Slava Gubar <sl.gubar@ukr.net>
2021-02-03 21:27:19 +0200

    Added more examples for manipulating with pointers/arrays.

--- DEMO/d01/d1/math.h
+++ DEMO/d01/d1/math.h
@@ -10,5 +10,13 @@
 
 #include <stdio.h>
 int sum(int a, int b);
+int matrix_sum(int **matrix, int row, int col);
+int matrix_sum1(int *matrix, int row, int col);
+
+int **create_matrix(int row, int col);
+void delete_matrix(int **matrix, int row);
+
+int *create_matrix1(int row, int col);
+void delete_matrix1(int *matrix);
 
 #endif /* math_h */
