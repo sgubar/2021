3b9d0e63cc9dcfb9353676fcaa4ba5ac23951be0
Unik6 <Unik6@users.noreply.github.com>
2021-02-03 17:31:40 +0200

    Homework1_update

---
+++ DK02/Вакуленко Максим/Homeworks/Дз1/dk_tool.h
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+#include<stdlib.h>
+#define lon_g 4
+#define lev 3 // Cтворюємо загалні змінні 
+#define DK_TOOL_H
+#ifdef DK_TOOL_H
+void num_mass(int num, int a[lev][lon_g]);
+void matr(int a[lev][lon_g]);
+void rand_arr(int arr[lev][lon_g]);
+                                    // Прототип функции
+
+#endif // !TEA_H
