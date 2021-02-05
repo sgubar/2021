1d40add8ec6ad99e1fecb53b5d4144f1e0db9330
mad03day <mad03day@users.noreply.github.com>
2021-02-05 19:01:53 +0200

    Оновлення

--- DK02/Remez Mishenka/ДЗ/1. Модуль математичних функцій/dk_tool.h
+++ DK02/Remez Mishenka/ДЗ/1. Модуль математичних функцій/dk_tool.h
@@ -1,7 +1,7 @@
 #include <stdio.h>		// Підключення бібліотек тут бо звик так робити :)
 #include <Windows.h>
 
-#ifdef DK_TOOL_H
 #define DK_TOOL_H
-	int limonchik(int* arr);		// Реально не запитуйте чого саме така назва
+#ifdef DK_TOOL_H
+	int Convert_Oct_to_Dec(char* arr);		
 #endif
--- DK02/Remez Mishenka/ДЗ/1. Модуль математичних функцій/main.c
+++ DK02/Remez Mishenka/ДЗ/1. Модуль математичних функцій/main.c
@@ -2,17 +2,18 @@
 
 #define MAX_NUMBER 4        // Розмір для задання масиву (на випадок якщо вам закортить попросити мене переробити код)
 
-char masive[MAX_NUMBER + 1], arr[MAX_NUMBER];
+
 
 
 int main(int argc, char* argv[])
 {
+    char masive[MAX_NUMBER + 1], arr[MAX_NUMBER];
     SetConsoleCP (1251);
     SetConsoleOutputCP (1251);
     printf ("Введите 8-миричное число: ");
     fgets (masive, 5, stdin);    
 
-    printf("%d\n", limonchik(masive));      //Вибачте за назву, але нічого краще в 11 годин вечора в голову не прийшло
+    printf("%d\n", Convert_Oct_to_Dec(masive));     
 
     system("pause");        // Пишу у віжуалці
     return 0;           // Повертаємо 0 як знак об успішності функції
