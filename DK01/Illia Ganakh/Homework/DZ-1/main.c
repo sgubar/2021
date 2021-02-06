997a3ef5f1e51bb43596d2a67090be7b6ed33c86
IlliaGx <71513045+IlliaGx@users.noreply.github.com>
2021-02-06 03:37:51 +0200

    Update main.c

--- DK01/Illia Ganakh/Homework/DZ-1/main.c
+++ DK01/Illia Ganakh/Homework/DZ-1/main.c
@@ -5,7 +5,7 @@
 int main() {
     int i, j;
     int row=0, col=0;
-    while(col < 2 && row < 2) { //перевірка на розмір масиву, дозволить зробити або одномірний(горизонтальний), або "вектор-стовпець"
+    while(col < 2 && row < 2) { //перевірка на розмір масиву, мінімум дозволить зробити або одномірний(горизонтальний), або "вектор-стовпець"
       printf("Number of columns in matrices:");
         scanf("%d", &col);
       printf("Number of rows in matrices:");
