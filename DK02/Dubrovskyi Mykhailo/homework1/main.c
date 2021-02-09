ca0f37190a206d49fb13d549e7ee32d7763d6fe2
michael-jebro <50410445+michael-jebro@users.noreply.github.com>
2021-02-10 00:22:27 +0200

    main.c added

---
+++ DK02/Dubrovskyi Mykhailo/homework1/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+#include "dk_tools.h"
+
+int main(int argc, char *argv[]) {
+	//test
+	printf("%d\n", get_chrs_pos(argv[1], argv[2], strlen(argv[1]), strlen(argv[2])));
+}
