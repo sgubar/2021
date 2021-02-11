75ee0901ed4d4fe44ea2ba9555cbe32b42a86c23
Unik6 <Unik6@users.noreply.github.com>
2021-02-11 23:51:47 +0200

    lab 1
    1

---
+++ DK02/Вакуленко Максим/lab 1/Hed.h
@@ -0,0 +1,13 @@
+#include<stdio.h> 
+#include<stdlib.h>
+// Cтворюємо загалні змінні 
+#define HED_H
+#ifdef HED_H
+int summa(int num_b);
+double power(int base, int pow);
+long factorial(int number);
+double q(int A, int B, int C);
+
+     // Прототип функции
+
+#endif // !TEA_H#pragma once
---
+++ DK02/Вакуленко Максим/lab 1/lab1.c
@@ -0,0 +1,29 @@
+﻿#include<stdio.h>
+#include"Hed.h"
+int main() {
+	int flag = 1;
+	do {
+	int A, B, C;
+	printf("Type a A: ");
+	scanf_s("%d", &A);
+
+	printf("Type a B (only >= 0): ");
+	scanf_s("%d", &B);
+	if (B < 0) {
+		printf("Fatal error enum < 0\n");
+		return 0;
+	}
+
+	printf("Type a C: ");
+	scanf_s("%d", &C);
+
+	printf("\n");
+
+    double Q = q(A, B, C);
+	printf("%lf\n", Q);
+	printf("Write 1 to start again: ");
+	scanf_s("%d", &flag);
+	} while (flag == 1);
+	printf("%lf\n", power(2, -2));
+	return 0;
+}
