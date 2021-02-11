f1eb89a0acf2159edad6acaece1979a57e66ca71
Unik6 <Unik6@users.noreply.github.com>
2021-02-12 00:21:21 +0200

    Update lab.1
    lab.1

--- DK02/Вакуленко Максим/lab 1/lab1.c
+++ DK02/Вакуленко Максим/lab 1/lab1.c
@@ -2,20 +2,30 @@
 #include"Hed.h"
 int main() {
 	int flag = 1;
-	do {
 	int A, B, C;
+	do {
+
 	printf("Type a A: ");
 	scanf_s("%d", &A);
+	if (A > 5000) {
+		printf("Error_A. Num is very big \n");
+		return 0;
+	}
 
 	printf("Type a B (only >= 0): ");
 	scanf_s("%d", &B);
-	if (B < 0) {
-		printf("Fatal error enum < 0\n");
+	if (B < 0 || B > 10) {
+		printf("Error_B. Enum < 0 or num is very big \n");
 		return 0;
 	}
 
 	printf("Type a C: ");
 	scanf_s("%d", &C);
+	if (C > 50) {
+		printf("Error_C. Num is very big \n");
+		return 0;
+	}
+	
 
 	printf("\n");
 
@@ -24,6 +34,6 @@ int main() {
 	printf("Write 1 to start again: ");
 	scanf_s("%d", &flag);
 	} while (flag == 1);
-	printf("%lf\n", power(2, -2));
+	//printf("%lf\n", power(2, -2));
 	return 0;
 }
