1b47c62a76135d17f2954fa66364fd465943c83f
michael-jebro <50410445+michael-jebro@users.noreply.github.com>
2021-02-12 18:13:03 +0200

    function and const declarations changed

--- DK02/Dubrovskyi Mykhailo/homework1/dk_tools.h
+++ DK02/Dubrovskyi Mykhailo/homework1/dk_tools.h
@@ -4,20 +4,30 @@
 // Enums
 enum Type {Int, Char, Long};
 
+// Error codes
+extern const int NOT_EXIST;
+extern const int NOT_A_TYPE;
+extern const int OVERSIZED;
+extern const int NOT_A_HEX;
+extern const int NOT_OCT;
+extern const int NOT_A_BIN;
+
+extern const int OK;
+
 //to do: matrixes
 
 int get_chrs_pos(char *charset, char *string, int chrs_buff_size, int str_buff_size);
 
-void * rearrange(void *array, int size, enum Type t);
+int rearrange(void *array, int size, enum Type t);
 
-int word_counter(char *array);
+int count_words(char *string, int str_buff_size);
 
-int count_chrs(char *charset, char *string);
+int count_chrs(char *charset, char *string, int chrs_buff_size, int str_buff_size);
 
-int base16_to_base10(char num[4]);
+int base16_to_base10(char *num);
 
-int base8_to_base10(char num[4]);
+int base8_to_base10(char *num);
 
-int base2_to_base10(char num[4]);
+int base2_to_base10(char *num);
 
 #endif
