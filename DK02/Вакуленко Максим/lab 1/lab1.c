2ebb901ccbfd07a0b7404354266f3414811e1326
Unik6 <Unik6@users.noreply.github.com>
2021-02-12 13:42:13 +0200

    upd lab 1
    lab 1

--- DK02/Вакуленко Максим/lab 1/lab1.c
+++ DK02/Вакуленко Максим/lab 1/lab1.c
@@ -5,24 +5,24 @@ int main() {
 	int A, B, C;
 	do {
 
-	printf("Type a A: ");
+	printf("Type a A (only > -5000  and < 5000): ");
 	scanf_s("%d", &A);
-	if (A > 5000) {
-		printf("Error_A. Num is very big \n");
+	if (A > 5000 || A < -5000) {
+		printf("Error_A. Num is very big or small \n");
 		return 0;
 	}
 
-	printf("Type a B (only >= 0): ");
+	printf("Type a B (only >=  and < 10): ");
 	scanf_s("%d", &B);
 	if (B < 0 || B > 10) {
 		printf("Error_B. Enum < 0 or num is very big \n");
 		return 0;
 	}
 
-	printf("Type a C: ");
+	printf("Type a C (only >  -50 and < 50): ");
 	scanf_s("%d", &C);
-	if (C > 50) {
-		printf("Error_C. Num is very big \n");
+	if (C > 50 || C > -50) {
+		printf("Error_C. Num is very big or small \n");
 		return 0;
 	}
 	
