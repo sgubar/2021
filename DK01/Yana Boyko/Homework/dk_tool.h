ee7aac6d6e5a37b2f018ea78ec104c1c3d81deae
Yanaboyko sty <72446363+YanaBoyko-ops@users.noreply.github.com>
2021-02-13 14:44:08 +0200

    Update dk_tool.h

--- DK01/Yana Boyko/Homework/dk_tool.h
+++ DK01/Yana Boyko/Homework/dk_tool.h
@@ -1,1 +1,12 @@
+#ifndef _DK_TOOL_H
+#define _DK_TOOL_H
 
+#include <stdio.h> 
+#include <stdlib.h>
+int** create_array(int rows, int cols);
+void array_up(int** array, int rows, int cols, int num_up);
+void delete_array(int** array, int rows);
+void print_array(int** array, int rows, int cols);
+void fill_array(int** array, int rows, int cols);
+
+#endif
