8b56b22adba74d5232cd68946b3bd6b20fc3f1c4
Yanaboyko sty <72446363+YanaBoyko-ops@users.noreply.github.com>
2021-02-13 14:34:59 +0200

    Update math.h

--- DK01/Yana Boyko/Laboratory works/lab 1/math.h
+++ DK01/Yana Boyko/Laboratory works/lab 1/math.h
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#ifdef math_h
+#define math_h 
 
+
+#define math.h
+double power(int base, int pow);
+long factorial(int base);
+double q(int A, int B, int C);
+
+#endif/*math_h*/
