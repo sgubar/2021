cd746994727ce6ec8e01f28b6382f7d25d2b3e0e
michael-jebro <50410445+michael-jebro@users.noreply.github.com>
2021-02-13 20:58:25 +0200

    ...

--- DK02/Dubrovskyi Mykhailo/homework1/dk_tools.h
+++ DK02/Dubrovskyi Mykhailo/homework1/dk_tools.h
@@ -6,15 +6,24 @@ enum Type {Int, Char, Long};
 
 // Error codes
 extern const int NOT_EXIST;
+extern const int NULLPASS;
+extern const int COLROW_ERR;
 extern const int NOT_A_TYPE;
 extern const int OVERSIZED;
 extern const int NOT_A_HEX;
 extern const int NOT_OCT;
 extern const int NOT_A_BIN;
+extern const int MATR_MULT_ERR;
 
 extern const int OK;
 
-//to do: matrixes
+int ** create_matrix(int columns, int rows);
+
+int delete_matrix(int **matrix, int rows);
+
+int ** sum_sub_matrixes(int **matrix1, int **matrix2, int rows, int columns);
+
+int ** mult_matrixes(int **matrix1, int **matrix2, int r1, int c1, int r2, int c2);
 
 int get_chrs_pos(char *charset, char *string, int chrs_buff_size, int str_buff_size);
 
