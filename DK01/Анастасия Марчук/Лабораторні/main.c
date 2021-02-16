b4a18a9efb519f75e9f8272454edd83dff9a8941
Marchuk-227 <72446411+Marchuk-227@users.noreply.github.com>
2021-02-16 21:07:32 +0200

    Create main.c

---
+++ DK01/Анастасия Марчук/Лабораторні/main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include"math.h"
+
+int main()
+{
+    int A, B, C;
+    {
+        printf("Enter parameter A:");
+        scanf("%d \n", &A);
+        printf("Enter parameter B:");
+        scanf("%d \n", &B);
+        printf("Enter parameter C:");
+        scanf("%d \n", &C);
+        printf("The result of calculation: %d\n", q(A; B; C));
+    }
+    
+}
