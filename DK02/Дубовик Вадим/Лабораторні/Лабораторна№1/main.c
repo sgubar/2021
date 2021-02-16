2beaa5199d24bc4c24df4398c0d8406f8a969208
0XXX0TNIK <72441080+0XXX0TNIK@users.noreply.github.com>
2021-02-16 23:43:49 +0200

    Add files via upload

---
+++ DK02/Дубовик Вадим/Лабораторні/Лабораторна№1/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "math.h"
+
+int main(int argc, const char* argv[])
+{
+	
+	int A, B, C;
+	printf("Dobrogo dnya, vy koristuetes programou\ndly rozrahunku formuly #5\n");
+	printf("Bud lasksa, vvedit chislo A,B,C:\n");
+    if (scanf_s("%d", &A) && scanf_s("%d", &B) && scanf_s("%d", &C))
+    {
+		printf("%f", Q_is(A, B, C));
+    }
+    else {
+        printf("Error: Chisla vvedeni nepravilno\n");        
+    }
+	
+}
+
