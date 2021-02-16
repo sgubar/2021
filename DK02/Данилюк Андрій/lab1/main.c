fcafe0074a7330c679b5e048c4517a379af24e64
MaestroHello <71627642+MaestroHello@users.noreply.github.com>
2021-02-16 21:45:01 +0200

    Create main.c

---
+++ DK02/Данилюк Андрій/lab1/main.c
@@ -0,0 +1,10 @@
+#include"dk_tool.h"
+
+int main()
+{
+	void get_numbers(int A, int B, int C);
+	double power(int A);
+	double progression(int A, int result);
+	double final_result = first_part + progression;
+	printf("Q = %d\n", final_result);
+}
