f5e967fc9adc33640384ece87f849a058da488d6
Yanaboyko sty <72446363+YanaBoyko-ops@users.noreply.github.com>
2021-02-22 19:26:01 +0200

    Update math.h

--- DK01/Yana Boyko/Laboratory works/lab 1/math.h
+++ DK01/Yana Boyko/Laboratory works/lab 1/math.h
@@ -6,6 +6,13 @@
 #define math.h
 double power(int base, int pow);
 long factorial(int base);
-double q(int A, int B, int C);
+double q(int A, int B, int C) {
+    int sigma = 0;
+    for (int D = 0; D <= B; D++){
+        sigma += factorial(D);
+    }
+    return A * B / (double)(B + power(C, 2)) + 1.0 * sigma;
+}
+
 
 #endif/*math_h*/
