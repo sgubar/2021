ef83740adbe091069699feecd58eb515cfe6508d
Slava Gubar <sl.gubar@ukr.net>
2021-02-23 18:14:26 +0200

    Added demo for polyline with dynamic array

--- DEMO/d02_hw/d02_hw/main.c
+++ DEMO/d02_hw/d02_hw/main.c
@@ -8,6 +8,8 @@
 #include <stdio.h>
 #include "line.h"
 #include "polyline.h"
+#include "arraypoints.h"
+#include "dpline.h"
 
 int main(int argc, const char * argv[]) {
 
@@ -55,11 +57,50 @@ int main(int argc, const char * argv[]) {
 	printPolyLine(line);
 	printf("len: %f\n", lenghtPolyLine(line));
 
+	printf("ArrayPoints test ...\n");
+
+	ArrayPoints *a = createArrayPoints(2);
+	printArrayPoints(a);
+
+	setPointAtIndex(a, p, 0);
+	printArrayPoints(a);
+
+	setPointAtIndex(a, p, 4);
+	printArrayPoints(a);
+
+	printf("Polyline test ...\n");
+	DPLine *dp = createDPLine();
+
+	addPointToDPLine(dp, p);
+	printDPLine(dp);
+	printf("len: %f\n", lenghtDPLine(dp));
+
+	addPointByXYToDPLine(dp, 7, 8);
+	printDPLine(dp);
+	printf("len: %f\n", lenghtDPLine(dp));
+
+	addPointByXYToDPLine(dp, 97, 78);
+	printDPLine(dp);
+	printf("len: %f\n", lenghtDPLine(dp));
+
+	addPointByXYToDPLine(dp, 87, 58);
+	printDPLine(dp);
+	printf("len: %f\n", lenghtDPLine(dp));
+
+	addPointByXYToDPLine(dp, 77, 88);
+	printDPLine(dp);
+	printf("len: %f\n", lenghtDPLine(dp));
+
+	addPointByXYToDPLine(dp, 37, 48);
+	printDPLine(dp);
+	printf("len: %f\n", lenghtDPLine(dp));
 	// insert code here...
 	printf("Hello, World!\n");
 	deletePoint(p);
 	deletePoint(copy);
 	deleteLine(l);
 	deletePolyLine(line);
+	deleteArrayPoints(a);
+	deleteDPLine(dp);
 	return 0;
 }
