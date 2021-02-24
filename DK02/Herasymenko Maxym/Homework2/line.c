d5c778e0baeba6dd940004a81b546d4515bd553d
Maxx67545 <72441324+Maxx67545@users.noreply.github.com>
2021-02-25 00:51:55 +0200

    Add files via upload

---
+++ DK02/Herasymenko Maxym/Homework2/line.c
@@ -0,0 +1,74 @@
+#include "line.h"
+#include <stdlib.h>
+#include <math.h>
+Line *createLineByPoints(Point *p1, Point *p2, Point *p3, Point *p4)
+{
+  if (NULL == p1 || NULL == p2)
+    {
+		return NULL;
+	}
+
+	Line *l = (Line *)malloc(sizeof(Line));
+	if (NULL != l) {
+		l->A = copyPoint(p1);
+		l->B = copyPoint(p2);
+		l->C = copyPoint(p3);
+		l->D = copyPoint(p4);
+	}
+
+	return l;
+}
+
+void deleteLine(Line *l)
+{
+    if (NULL != l)
+    {
+		deletePoint(l->A);
+		deletePoint(l->B);
+		free(l);
+	}
+}
+
+void printLine(Line *l)
+{
+  if (NULL != l)
+    {
+		printPoint(l->A);
+		printf("-");
+		printPoint(l->B);
+		printf("-");
+		printPoint(l->C);
+		printf("-");
+		printPoint(l->D);
+		printf("\n");
+	}
+}
+
+
+double lenghtLine(Line *l)//функція визначення довжини між точками
+{
+	if (NULL == l)
+    {
+		return -1;
+	}
+
+	long dX = l->A->x - l->B->x;
+	long dY = l->A->y - l->B->y;
+
+	return sqrt(dX*dX + dY*dY);
+}
+
+#define MOD(n) (n < 0 ? -n : n)
+
+double power(int base, int pow) //потрібен степень для знаходження площі квадрата
+{
+
+	double result = 1;
+	int pw = MOD(pow);
+	for (int i = 0; i < pw; i ++)
+    {
+		result *= base;
+	}
+
+	return (pow < 0) ? 1.0 / result : result;
+}
