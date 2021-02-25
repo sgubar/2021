6c381124997472944668e7ec72ed60641f620842
OpolinK <71596274+OpolinK@users.noreply.github.com>
2021-02-25 20:52:00 +0200

    Add files via upload

---
+++ DK01/Sukhanov Danya/LabWork/cal.h
@@ -0,0 +1,12 @@
+#ifndef cal_h
+#define cal_h
+
+#include <stdio.h> 
+#include <stdlib.h>
+
+double power(int base, int pow);
+long mod(int A, int B, int C);
+long sum(int start, int finish);
+float q(int A, int B, int C);
+
+#endif
