49738a5bd9559e46eda142619a157f46c6500180
Unik6 <Unik6@users.noreply.github.com>
2021-03-09 01:48:52 +0200

    lab 2
    gg

---
+++ DK02/Вакуленко Максим/lab 2/lab2.c
@@ -0,0 +1,33 @@
+﻿
+#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include"list.h"
+
+int main() {
+	FILE* fPointer;
+	fopen_s(&fPointer, "sample.txt", "r");
+	char sumb;
+	if (fPointer == NULL) {
+		printf("Error. fPointer == NULL\n");
+		return 0;
+	}
+
+	DoubleCharList* sample = createDoubleCharList();
+	while ((sumb = fgetc(fPointer)) != EOF) {
+
+		addCharValueToDoubleCharList(sample, sumb);
+
+		if (sumb == '\n' || sumb == '\0') {
+			printRevDoubleCharList(sample);
+			reFreshDoubleCharList(sample);
+		}
+
+	}
+	printf("\n");
+	printRevDoubleCharList(sample);
+	deleteDoubleCharList(sample);
+
+	fclose(fPointer);
+	return 0;
+}
