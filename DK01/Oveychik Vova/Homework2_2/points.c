9cc5dcec6786b1d716ad69c438aeab9e24a71020
vova-oveychick <71566228+vova-oveychick@users.noreply.github.com>
2021-03-10 20:46:03 +0200

    Add files via upload

---
+++ DK01/Oveychik Vova/Homework2_2/points.c
@@ -0,0 +1,37 @@
+#include "points.h"
+#include <stdlib.h>
+
+Point *createPoint(int x, int y) {
+	Point *point = (Point *)malloc(sizeof(Point));
+	if (NULL != point) {
+		point->x = x;
+		point->y = y;
+	}
+
+	return point;
+}
+
+void deletePoint(Point *p) {
+	if (NULL != p) {
+		free(p);
+	}
+}
+
+Point *copyPoint(Point *p) {
+	if (NULL == p) {
+		return NULL;
+	}
+
+	return createPoint(p->x, p->y);
+}
+
+void printPoint(Point *p) {
+	if (NULL == p) {
+		return;
+	}
+
+	printf("(%d, %d)", p->x, p->y);
+}
+
+
+
