e08868df57d49c6d492fe828761191e7488f2114
OrestShvets <71658599+OrestShvets@users.noreply.github.com>
2021-03-22 18:48:08 +0200

    Add files via upload

---
+++ DK02/Orest Shvets/lab2/list.h
@@ -0,0 +1,27 @@
+#ifndef list_h
+#define list_h
+#include <stdio.h>
+typedef struct tagIntListElement IntListElement;
+struct tagIntListElement
+{
+	int value;
+	IntListElement* prev;
+	IntListElement* next;
+}
+;
+IntListElement* createIntListElement(int value);
+void deleteIntListElement(IntListElement* element);
+void printIntListElement(IntListElement* element);
+typedef struct tagIntList
+{
+	IntListElement* head;
+	IntListElement* tail;
+}
+IntList;
+IntList* createIntList(void);
+void deleteIntList(IntList* list);
+int addIntValueToIntList(IntList* list, int value);
+void addRandomValuesToList(IntList* list, int count);
+void printIntList(IntList* list);
+void printRIntList(IntList* list);
+#endif
