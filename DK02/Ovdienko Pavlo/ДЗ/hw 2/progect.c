62b2f4dd1c90a6ec623b259ff224d58d2687cf8d
leader1gnite <68400635+leader1gnite@users.noreply.github.com>
2021-03-23 16:06:23 +0200

    update

--- DK02/Ovdienko Pavlo/ДЗ/hw 2/progect.c
+++ DK02/Ovdienko Pavlo/ДЗ/hw 2/progect.c
@@ -1,14 +1,14 @@
-#include "RectangleArray.h"
+#include "Rectangle.h"
 
 
 int main(int argc, const char* argv[]) {
 
-	RectangleArray* wow = createRectangleArray(10);
-	printRectangleArray(wow);
+
 	Rectangle* rec = createRectangleByCoords(0, 0, 0, 4, 6, 4, 6, 0);
 	Rectangle* res = createRectangleByCoords(0, 0, 0, 4, 6, 4, 6, 0);
-	setRectangleAtIndex(wow, rec, 0);
-	addRectangleToRectangleArray(wow, res);
-	printRectangleArray(wow);
-	deleteRectangleArray(wow);
+	printRectangle(rec);
+	printRectangle(res);
+	isEqualRectangle(rec, res);
+	deleteRectangle(rec);
+	deleteRectangle(res);
 }
