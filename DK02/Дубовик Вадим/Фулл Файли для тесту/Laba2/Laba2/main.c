0fb2033c8f188133ed8895af6640202372ab1f8a
mad03day <71565506+mad03day@users.noreply.github.com>
2021-03-23 16:03:08 +0200

    /

--- DK02/Дубовик Вадим/Фулл Файли для тесту/Laba2/Laba2/main.c
+++ DK02/Дубовик Вадим/Фулл Файли для тесту/Laba2/Laba2/main.c
@@ -2,22 +2,13 @@
 
 int main(void)
 {
-	int B = 10;
-	int* C = &B;
-	
-	pListEl A = createListEl(10);
-	printf("%d", A->value);
-	free(C);
-	//deleteListEl(A);
-
-
 
 	int amountOfElements;
-	pListEl list = NULL;
+	
 	printf("Please, enter the lenght of your list.\n ");
-	list = createListWithXEl(amountOfElements = getNum());
+	pList list = createListWithXEl(amountOfElements = getNum());
 	printList(list);
 
-	//deleteFirstElLessThanValue(list, findAverage(list));
-	//printList(list);
+	deleteFirstElLessThanValue(list, findAverage(list));
+	printList(list);
 }
