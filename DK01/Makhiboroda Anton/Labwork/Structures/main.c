f3110f2f125c0f2b351be02ad87053bac4c979a5
Sonikotik <71600499+Sonikotik@users.noreply.github.com>
2021-03-26 16:02:51 +0200

    Add files via upload

---
+++ DK01/Makhiboroda Anton/Labwork/Structures/main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "List.h"
+int main()
+{
+
+    printf("Vas vitae programa z pidrahynky formylu: A!/|2+A*B| \n");
+    printf("\n");
+    printf("/|................Yvaga!!......................|\ \n");
+    printf("|| Mojna vvodutu chisla vid 0 do 1000          || \n");
+    printf("|| Mojna vvodutu kilkist chisel do 100         || \n");
+    printf("||   Vci chisla povunni bytu NATURALNI         || \n");
+    printf("\\|.............................................|/\n");
+    printf("\n");
+    printf("Programy stvoruv student DK-01 Makhiboroda Anton\n");
+    printf("\n");
+Yes: ;
+    IntListElement *element1 = createIntListElement(5);
+
+    printf("\n");
+
+    IntList *list = createIntList();
+
+    int b=0;
+
+    printf("Vvedit kilkist chisel: ");
+    scanf("%d", &b);
+    if ( 0 > b || b > 100 ) goto Err;
+     for (int i=0,a=0; i <b; i++)
+        {
+            if ( 0 > a || a > 1000 ) goto Err;
+              printf("Vvedit chislo %d: ",i+1);
+              scanf("%d", &a);
+              addIntValueToIntList(list,a);
+        }
+    printf("\n");
+
+    printIntList(list);
+
+    printf("\nMax =");
+    printIntListElement( MaxListElement(list));
+    printf("\n");
+
+    SortForHeadToMax(list);
+    printIntList(list);
+
+   deleteIntListElement(element1);
+    deleteIntList(list);
+
+
+    Err: ;
+        char tryAgain;
+    printf("Ne korekto bulu vvedeni chusla!");
+    printf("\n Wanna try again? (y/n)");
+      scanf("%s", &tryAgain);
+      if(tryAgain=='y' || tryAgain=='Y') goto Yes;
+
+
+    return 0;
+
+}
