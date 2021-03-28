31ae77edd01afed9060037abe7e963a2701619cb
Yanaboyko sty <72446363+YanaBoyko-ops@users.noreply.github.com>
2021-03-28 13:56:19 +0300

    Create triangle.h

---
+++ DK01/Yana Boyko/HW3/triangle.h
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "line.h"
+
+#define triangle_h
+#ifdef triangle_h
+
+typedef struct
+{
+    Line *line_1;
+
+    Line *line_2;
+
+    Line *line_3;
+
+} Tria;
+
+Tria *createTriangleByPoints(PtCord *point1, PtCord *point2, PtCord *point3);
+
+int TestOfLife(PtCord *point1, PtCord *point2, PtCord *point3);
+
+void deleteTria(Tria *l);
+
+double PerimeterOfTriangle(Tria *Per);
+
+double SquareOfTriangle(Tria *Per);
+
+void printTriange(Tria* test);
+
+void DrawTriangle();
+
+Tria *copyTria(Tria *t);
+
+
+#endif // !TEA_H
