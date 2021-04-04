90a32d5adc8a8a0ad7d581d146e6d1404df08e7c
Unik6 <Unik6@users.noreply.github.com>
2021-04-04 23:22:23 +0300

    hw4
    hw4

---
+++ DK02/Вакуленко Максим/Homeworks/hw4/main.c
@@ -0,0 +1,32 @@
+﻿#include<stdio.h>
+#include "list.h"
+
+int main() {
+	List* test = createList();
+	printf("1/n");
+	addIntToList(test, 0);
+	addIntToList(test, 1);
+	addIntToList(test, 2);
+	addIntToList(test, 3);
+	addIntToList(test, 4);
+	addIntToList(test, 5);
+	addIntToList(test, 6);
+	addIntToList(test, 7);
+	addIntToList(test, 8);
+	printIntSingleList(test);
+
+	printf("2/n");
+	removeFirstElementFromIntList(test);
+	removeLastElementFromList(test);
+	printIntSingleList(test);
+	printf("3/n");
+
+	insertElementToListByIndex(test, 1, 10);
+	removeElementFromListByIndex(test, 2);
+	printIntSingleList(test);
+	
+	
+
+	deleteList(test);
+
+}
