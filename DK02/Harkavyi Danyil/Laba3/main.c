85d30ba98ca3a79a9d8ab4e35b29833105e1b147
HarkavyiDanyil <71595238+HarkavyiDanyil@users.noreply.github.com>
2021-04-10 02:57:58 +0300

    laba3

---
+++ DK02/Harkavyi Danyil/Laba3/main.c
@@ -0,0 +1,49 @@
+﻿#include <stdio.h>
+#include "tree.h"
+
+int main(int argc, const char* argv[]) {
+
+	IntTree* tree = createIntTree();
+
+	insertIntValueToTree(tree, 15);
+
+	insertIntValueToTree(tree, 7);
+
+	insertIntValueToTree(tree, 4);
+
+	insertIntValueToTree(tree, 3);
+
+	insertIntValueToTree(tree, 9);
+
+	insertIntValueToTree(tree, 20);
+
+	insertIntValueToTree(tree, 16);
+
+	insertIntValueToTree(tree, 48);
+
+	insertIntValueToTree(tree, 50);
+
+	printTree(tree);
+
+	deleteNodeWithValue(tree, 3);
+
+	deleteNodeWithValue(tree, 50);
+
+	printf("\n");
+
+	printTree(tree);
+
+	destroyIntTree(tree);
+
+	return 0;		 
+}
+    //			    (15)
+	//			   /    \
+	//		     (7)   (20)
+	//		    /  \    /  \
+	//		 (4)   (9) (16) (48)
+	//		 /                \
+	//	   (3)	              (50)
+	//		 
+	//		 
+
---
+++ DK02/Harkavyi Danyil/Laba3/tree.h
@@ -0,0 +1,36 @@
+#pragma once
+#ifndef tree_h
+#define tree_h
+
+#include <stdio.h>
+
+typedef struct tagNode
+{
+	int value;
+	struct tagNode* leftChild;
+	struct tagNode* rightChild;
+}IntNode;
+
+typedef struct tagTree
+{
+	IntNode* root;
+	int count;
+} IntTree;
+
+
+IntTree* createIntTree();
+
+void destroyIntTree(IntTree* aTree);
+
+void insertIntValueToTree(IntTree* aTree, int aValue);
+
+IntNode* findNodeWithValue(IntTree* aTree, int aValue);
+
+void deleteNodeWithValue(IntTree* aTree, int aValue);
+
+
+void printTree(IntTree* aTree);
+
+int countNodesWithTree(IntTree* aTree);
+
+#endif /* tree_h */
