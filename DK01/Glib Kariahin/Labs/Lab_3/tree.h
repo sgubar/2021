75086ee1cfb96256146c90ba33674314e9b3d6e0
glib2002 <glebix@ukr.net>
2021-04-11 20:46:11 +0300

    Create tree.h

---
+++ DK01/Glib Kariahin/Labs/Lab_3/tree.h
@@ -0,0 +1,34 @@
+
+
+#ifndef tree_h
+#define tree_h
+
+#include <stdio.h>
+
+typedef struct __tagNode
+{
+    double value;
+    struct __tagNode *leftChild;
+    struct __tagNode *rightChild;
+} DoubleNode;
+
+typedef struct __tagTree
+{
+    DoubleNode *root;
+    int count;
+} DoubleTree;
+
+// interface
+DoubleTree *createDoubleTree();
+void destroyDoubleTree(DoubleTree *aTree);
+
+void insertDoubleValueToTree(DoubleTree *aTree, double aValue);
+DoubleNode *findNodeWithValue(DoubleTree *aTree, double aValue);
+void deleteNodeWithValue(DoubleTree *aTree, double aValue);
+
+//void mergeTrees(DoubleTree *aTreeDst, DoubleTree *aTreeSrc);
+
+void printTree(DoubleTree *aTree);
+int countNodesWithTree(DoubleTree *aTree);
+
+#endif /* tree_h */
