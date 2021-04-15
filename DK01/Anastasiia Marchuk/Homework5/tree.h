969fec8629ce57a81f16c561f6fc3a6756869b29
Marchuk-227 <72446411+Marchuk-227@users.noreply.github.com>
2021-04-16 01:01:38 +0300

    Create tree.h

---
+++ DK01/Anastasiia Marchuk/Homework5/tree.h
@@ -0,0 +1,33 @@
+#ifndef tree_h
+#define tree_h
+
+#include <stdio.h>
+
+typedef struct tagNode
+{
+	char sumbol;
+	struct tagNode *leftChild;
+	struct tagNode *rightChild;
+} CharNode;
+
+typedef struct tagTree
+{
+	CharNode *root;
+
+	int count;
+} CharTree;
+
+
+CharTree *createCharTree();
+void delateCharTree(CharTree *aTree);
+
+void insertCharSumbolToTree(CharTree *aTree, char aSumbol);
+CharNode *findNodeWithSumbol(CharTree *aTree, char aSumbol);
+void deleteNodeWithSumbol(CharTree *aTree, char aSumbol);
+
+
+
+void printTree(CharTree *aTree);
+
+
+#endif
