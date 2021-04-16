db9f4f208774fbb517396b47150c68d7363652ff
Unik6 <61600190+Unik6@users.noreply.github.com>
2021-04-16 15:24:46 +0300

    Lab 3
    gg

---
+++ DK02/Вакуленко Максим/lab 3/tree.c
@@ -0,0 +1,262 @@
+#include "tree.h"
+#include <stdlib.h>
+
+static void destroyNode(IntNode* aNode);
+static IntNode* createIntNodeWithValue(int aValue);
+static IntNode* getSuccessor(IntTree* tree, IntNode* toDelete);
+static void print_int_node(IntNode* node);
+
+IntTree* createIntTree()
+{
+	IntTree* theTree = (IntTree*)malloc(sizeof(IntTree));
+
+	if (NULL != theTree)
+	{
+		theTree->root = NULL;
+		theTree->count = 0;
+	}
+
+	return theTree;
+}
+
+void destroyIntTree(IntTree* aTree)
+{
+	if (NULL != aTree)
+	{
+		destroyNode(aTree->root);
+		free(aTree);
+	}
+}
+
+void insertIntValueToTree(IntTree* aTree, int aValue)
+{
+	if (NULL == aTree)
+	{
+		return;
+	}
+
+	IntNode* theNode = createIntNodeWithValue(aValue);
+	if (NULL == theNode)
+	{
+		return;
+	}
+
+	if (NULL == aTree->root)
+	{
+		aTree->root = theNode;
+		aTree->count++;
+	}
+	else
+	{
+		IntNode* theCurrent = aTree->root;
+		IntNode* theParent = NULL;
+
+		while (1)
+		{
+			theParent = theCurrent;
+
+			if (aValue < theCurrent->value)
+			{
+				theCurrent = theCurrent->leftChild;
+				if (NULL == theCurrent)
+				{
+					theParent->leftChild = theNode;
+					break;
+				}
+			}
+			else
+			{
+				theCurrent = theCurrent->rightChild;
+				if (NULL == theCurrent)
+				{
+					theParent->rightChild = theNode;
+					break;
+				}
+			}
+		}
+
+		aTree->count++;
+	}
+}
+
+IntNode* findNodeWithValue(IntTree* aTree, int aValue)
+{
+	IntNode* theCurrentNode = NULL;
+
+	if (NULL != aTree && NULL != aTree->root)
+	{
+		 theCurrentNode = aTree->root; //<! - start from root
+		while (aValue != theCurrentNode->value) //<! - walk through the tree
+		{
+			theCurrentNode = (aValue < theCurrentNode->value)
+				? theCurrentNode->leftChild
+				: theCurrentNode->rightChild;
+
+			if (NULL == theCurrentNode)
+			{
+				break;
+			}
+		}
+	}
+
+	return theCurrentNode;
+}
+
+void deleteNodeWithValue(IntTree* aTree, int aValue) {
+
+
+	if (NULL == aTree || NULL == aTree->root) {
+		return;
+	}
+
+	IntNode* current = aTree->root;
+	IntNode* parent = aTree->root;
+
+
+	while (aValue != current->value) {
+		parent = current;
+		if (aValue < current->value) {
+			current = current->leftChild;
+		}
+		else {
+			current = current->rightChild;
+		}
+
+		if (NULL == current) {
+			return;
+		}
+	}
+
+
+	if (NULL == current->leftChild && NULL == current->rightChild) { // якщо вузол не маЇ нащадк≥в
+		if (aTree->root == current) {
+			aTree->root = NULL;
+		}
+		else if (parent->leftChild == current) {
+			parent->leftChild = NULL;
+		}
+		else {
+			parent->rightChild = NULL;
+		}
+
+		aTree->count--;
+	}
+
+	else if (NULL == current->rightChild) { //€кщо вузол маЇ л≥вого нащадка
+		if (aTree->root == current) {
+			aTree->root = current->leftChild;
+		}
+		else if (parent->leftChild == current) {
+			parent->leftChild = current->leftChild;
+		}
+		else {
+			parent->rightChild = current->rightChild;
+		}
+	}
+
+	else if (NULL == current->leftChild) { // €кщо вузол маЇ правого нащадка
+		if (aTree->root == current) {
+			aTree->root = current->rightChild;
+		}
+		else if (parent->rightChild == current) {
+			parent->rightChild = current->rightChild;
+		}
+		else {
+			parent->leftChild = current->leftChild;
+		}
+	}
+
+	else {    //якщо вузол маЇ двох нащадк≥в
+		IntNode* successor = getSuccessor(aTree, current);
+		if (aTree->root == successor) {
+			aTree->root = NULL;
+		}
+		else if (parent->leftChild == current) {
+			parent->leftChild = successor;
+		}
+		else {
+			parent->rightChild = successor;
+		}
+		current->leftChild = NULL;
+		current->rightChild = NULL;
+	}
+
+	destroyNode(current);
+}
+
+
+void printTree(IntTree* aTree) {
+	IntNode* item = aTree->root;
+	print_int_node(item);
+}
+
+void print_int_node(IntNode* node) {
+	if (node == NULL)
+		return;
+	print_int_node(node->leftChild);
+	printf("Elenemt: %d\n", node->value);
+	print_int_node(node->rightChild);
+}
+
+
+void destroyNode(IntNode* aNode)
+{
+	if (NULL != aNode)
+	{
+		destroyNode(aNode->leftChild);
+		destroyNode(aNode->rightChild);
+
+		free(aNode);
+	}
+}
+
+IntNode* createIntNodeWithValue(int aValue)
+{
+	IntNode* theNode = (IntNode*)malloc(sizeof(IntNode));
+
+	if (NULL != theNode)
+	{
+		theNode->leftChild = NULL;
+		theNode->rightChild = NULL;
+		theNode->value = aValue;
+	}
+
+	return theNode;
+}
+
+IntNode* getSuccessor(IntTree* tree, IntNode* toDelete) {
+	IntNode* successParent = toDelete;
+	IntNode* successor = toDelete;
+	IntNode* current = toDelete->rightChild;
+
+	while (NULL != current) {
+		successParent = successor;
+		successor = current;
+		current = current->leftChild;
+	}
+	 
+
+
+	if (successor != toDelete->rightChild) { 
+		successParent->leftChild = successor->rightChild; //39 -> NULL
+		successor->rightChild = toDelete->rightChild; //35->39
+	}
+
+	successor->leftChild = toDelete->leftChild;
+
+	return successor;
+}
+
+
+ 
+ /*	    
+      (23)
+	 /    \
+	/      \
+ (12)	     (32)
+ / \          / \
+(9) (15)    (15) (47)
+ /                 \
+(3)                (34)    
+ */
+ 
