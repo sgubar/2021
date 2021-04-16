2d64f010662ca74b9bed31d8770296d1d63aaf9c
Unik6 <61600190+Unik6@users.noreply.github.com>
2021-04-16 15:25:30 +0300

    hw5
    hw5

---
+++ DK02/Вакуленко Максим/Homeworks/hw5/main.c
@@ -0,0 +1,57 @@
+﻿// tree.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <stdio.h>
+#include"tree.h"
+
+int main(){
+    
+    IntTree* tree = createIntTree();
+
+
+
+
+
+
+	insertIntValueToTree(tree, 23);
+
+	insertIntValueToTree(tree, 12);
+
+	insertIntValueToTree(tree, 15);
+
+	insertIntValueToTree(tree, 9);
+
+	insertIntValueToTree(tree, 3);
+
+	insertIntValueToTree(tree, 27);
+
+	insertIntValueToTree(tree, 32);
+
+	insertIntValueToTree(tree, 47);
+
+	insertIntValueToTree(tree, 56);
+
+
+	
+
+
+
+	printf("\n");
+
+	printTree(tree);
+
+	deleteNodeWithValue(tree, 12);
+//
+	deleteNodeWithValue(tree, 3);
+
+	deleteNodeWithValue(tree, 34);
+
+	printf("\n");
+
+	printTree(tree);
+
+	destroyIntTree(tree);
+
+	return 0;		 
+
+}
---
+++ DK02/Вакуленко Максим/Homeworks/hw5/tree.h
@@ -0,0 +1,33 @@
+#ifndef tree_h
+#define tree_h
+
+#include <stdio.h>
+
+typedef struct __tagNode
+{
+	int value;
+	struct __tagNode *leftChild;
+	struct __tagNode *rightChild;
+} IntNode;
+
+typedef struct __tagTree
+{
+	IntNode *root;
+	int count;
+} IntTree;
+
+// interface
+IntTree *createIntTree();
+void destroyIntTree(IntTree *aTree);
+
+void insertIntValueToTree(IntTree *aTree, int aValue);
+IntNode *findNodeWithValue(IntTree *aTree, int aValue);
+void deleteNodeWithValue(IntTree *aTree, int aValue);
+
+void mergeTrees(IntTree *aTreeDst, IntTree *aTreeSrc);
+
+void printTree(IntTree *aTree);
+int countNodesWithTree(IntTree *aTree);
+
+void printIntTree(IntTree** aTree);
+#endif /* tree_h */
