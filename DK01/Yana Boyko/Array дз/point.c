ca017959db1cfca6074dc8dc5c7f467f30d99c04
Yanaboyko sty <72446363+YanaBoyko-ops@users.noreply.github.com>
2021-05-03 14:24:46 +0300

    Create point.c

---
+++ DK01/Yana Boyko/Array дз/point.c
@@ -0,0 +1,50 @@
+#include "point.h"
+
+PtCord *createPoint(int x, int y)
+{
+    PtCord *point = (PtCord *)malloc(sizeof(PtCord));
+    if (NULL != point)
+    {
+        point->x = x;
+
+        point->y = y;
+    }
+    return point;
+}
+
+void deletePoint(PtCord *p)
+{
+    if (NULL != p)
+    {
+        free(p);
+    }
+}
+
+
+void printPoint(PtCord *p)
+{
+    if (NULL == p)
+    {
+        return;
+    }
+    printf("(%d, %d)", p->x, p->y);
+}
+
+
+PtCord *copyPoint(PtCord *p)
+{
+    if (NULL == p)
+    {
+        return NULL;
+    }
+    return createPoint(p->x, p->y);
+}
+
+int isEqualPoints(PtCord *lp, PtCord *rp)
+{
+    if (NULL == lp || NULL == rp)
+    {
+        return 0;
+    }
+    return (lp == rp || (lp->x == rp->x && lp->y == rp->y)) ? 1 : 0;
+}
