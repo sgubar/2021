1fe08111f4550a77cda9a02ca2bdca6f1755f98c
Myron <48879773+Myronchik@users.noreply.github.com>
2021-06-01 00:31:42 +0300

    Create code_01.c

---
+++ DK01/Myron Danyliv/DZ_01/code_01.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+void suma(int N, int A[][N], int B[][N], int RESULT[][N]){
+	                                                       int i=0;
+	                                                       while (i<N){
+		                                                                int j=0;
+		                                                                while (j<N){
+			                                                                         RESULT[i][j] = A[i][j] + B[i][j];
+			                                                                         j++;
+		                                                                           }
+		                                                                i++;
+	                                                                  }
+                                                         }
