42919e9d8a353c0fbda387dfab5864d1ff201e75
Myron <48879773+Myronchik@users.noreply.github.com>
2021-06-01 00:46:40 +0300

    Create code.c

---
+++ DK01/Myron Danyliv/DZ_04/code.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "code_1.h"
+
+int main() {
+           T_ListNode* singleLinkedList = CreateList(1);
+           AddToList(singleLinkedList, 2);
+           AddToList(singleLinkedList, 2);
+           AddToList(singleLinkedList, 3);
+           AddToList(singleLinkedList, 4);
+           AddToList(singleLinkedList, 5);
+           AddToList(singleLinkedList, 6);
+           AddToList(singleLinkedList, 6);
+           AddToList(singleLinkedList, 2.1);
+           AddToList(singleLinkedList, 23.5);
+
+           PrintList(singleLinkedList);
+
+           InsertByIndex(singleLinkedList, 5, 100);
+
+           PrintList(singleLinkedList);
+
+           RemoveFromListByIndex(singleLinkedList, 8);
+
+           PrintList(singleLinkedList);
+
+           printf("Final list size: %u\n",ListSize(singleLinkedList));
+
+           DestroyList(singleLinkedList);
+           }
