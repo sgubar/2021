43519f1d4e1eb698a244065fdc8cdc67f703a294
AntonIvlev1 <71598939+AntonIvlev1@users.noreply.github.com>
2021-06-01 14:09:33 +0300

    Create lib.c

---
+++ DK02/Івлев Антон/дз/дз5/lib.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lib.h"
+
+t_list *create_node(int set_i){
+
+    t_list *node = (t_list *)malloc(sizeof(t_list));
+
+        node -> i =set_i;
+
+        node -> left = NULL;
+        node -> right = NULL;
+
+        return node;
+}
+
+void push(t_list **list, int set_i){
+        t_list *new_element = create_node(set_i);
+        t_list *tmp = *list;
+        while(tmp->left !=0 || tmp->right!=0){
+            if(tmp->i >new_element->i)
+                tmp=tmp->left;
+            if(tmp->i < new_element->i)
+                tmp=tmp->right;
+        }
+        if(tmp->i >new_element->i)
+                tmp->left=new_element;
+        if(tmp->i < new_element->i)
+                tmp->right=new_element;
+}
+
+void treeprint(tnode *tree) {
+  if (tree!=NULL) { 
+    printf("%i \n",tree->field); 
+    treeprint(tree->left); 
+    treeprint(tree->right); 
+  }
+}
+
+void freedel(tnode *tree) {
+  if(tree!=NULL) {
+    freedel(tree->left);
+    freedel(tree->right);
+    free(tree);
+  }
+}
