c00104898ba688df4f7ac0964cf0ba434569a329
AntonIvlev1 <71598939+AntonIvlev1@users.noreply.github.com>
2021-06-01 14:22:55 +0300

    Update main.c

--- DK02/Івлев Антон/дз/дз5/main.c
+++ DK02/Івлев Антон/дз/дз5/main.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include "lib.h"
 
+typedef struct s_list {
+    int i;
+
+    struct s_list *left;
+    struct s_list *right;
+}t_list;
+
 int main(){
     t_list *list = create_node(4);
     push(&list, 5);
