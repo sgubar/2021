cb8c90674a6460338e87f9557aa74a2e6fe7c1e0
AntonIvlev1 <71598939+AntonIvlev1@users.noreply.github.com>
2021-06-06 05:59:44 +0300

    Create lib.h

---
+++ DK02/Івлев Антон/Лаб/laba2/lib.h
@@ -0,0 +1,8 @@
+#pragma once
+
+t_list *create_node(int set_i);
+void push_front(t_list **list, int set_i);
+void push_back(t_list **list, int set_i);
+void del(t_list **list, int r);
+void dell(t_list **list);
+void lab(t_list **list);
