6ac2414709441eb84c6f1485c3c1f0a54aefe3d0
Marchuk-227 <72446411+Marchuk-227@users.noreply.github.com>
2021-06-15 23:54:50 +0300

    Update list.h

--- DK01/Anastasiia Marchuk/Homework4/list.h
+++ DK01/Anastasiia Marchuk/Homework4/list.h
@@ -26,14 +26,14 @@ void deleteDoubleList(DoubleList *list);
 void printDoubleList(DoubleList *list);
 
 long countOfDoubleList(DoubleList *list);
-int addDoubleValueToDoubleList(DoubleList *list, double value);
+double addDoubleValueToDoubleList(DoubleList *list, double value);
 void removeLastElementFromDoubleList(DoubleList *list);
 void removeFirstElementFromDoubleList(DoubleList *list);
 
 
-int insertDoubleElementToDoubleListAtIndex(DoubleList *list, double index, double value);
+double insertDoubleElementToDoubleListAtIndex(DoubleList *list, double index, double value);
 
-int removeDoubleElementFromDoubleListAtIndex(DoubleList *list, double index);
+double removeDoubleElementFromDoubleListAtIndex(DoubleList *list, double index);
 
 
 #endif
