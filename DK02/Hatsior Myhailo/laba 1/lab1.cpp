5e88f394d3b3a00de5a20f282ab939c43fc31363
mishahatsior <71599780+mishahatsior@users.noreply.github.com>
2021-06-15 22:47:30 +0300

    Add files via upload

---
+++ DK02/Hatsior Myhailo/laba 1/lab1.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+using namespace std;
+
+int fact(int number)
+{
+	if (number == 0)
+	{
+		return 1;
+	}
+	else
+	{
+		return number * fact(number -1);
+	}
+}
+
+int main()
+{
+	int A, B, C, D;
+	double Q;
+
+	while (true)
+	{
+		cout << "Enter A: "; cin >> A;
+		if (!cin)
+		{
+			cout << "entered data is not correct, try again!" << endl;
+			cin.clear();
+			while (cin.get() != '\n');
+		}
+		else break;
+	}
+
+	while (true)
+	{
+		cout << "Enter B: "; cin >> B;
+		if (!cin)
+		{
+			cout << "entered data is not correct, try again!" << endl;
+			cin.clear();
+			while (cin.get() != '\n');
+		}
+		else break;
+	}
+
+	while (true)
+	{
+		cout << "Enter C: "; cin >> C;
+		if (!cin)
+		{
+			cout << "entered data is not correct, try again!" << endl;
+			cin.clear();
+			while (cin.get() != '\n');
+		}
+		else break;
+	}
+
+	if (B + C * C == 0)
+	{
+		cout << "Undefined" << endl;
+		return 0;
+	}
+
+	Q = (A * B) / (B + C * C);
+	for (int i = 0; i <= B; i++)
+	{
+		Q += fact(i);
+	}
+
+	cout.setf(ios::fixed);
+	cout << "Q = " << Q << endl;
+}
