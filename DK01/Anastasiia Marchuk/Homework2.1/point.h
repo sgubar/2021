f6b521a38fd694bc9b6a25226608968fc9b78890
Marchuk-227 <72446411+Marchuk-227@users.noreply.github.com>
2021-06-16 06:28:54 +0300

    Create point.h

---
+++ DK01/Anastasiia Marchuk/Homework2.1/point.h
@@ -0,0 +1,17 @@
+#ifndef point_h
+#define point_h
+
+#include <stdio.h>
+
+typedef struct tag_point {
+	int x;
+	int y;
+} Point;
+
+Point *createPoint(int x, int y);
+void deletePoint(Point *p);
+void printPoint(Point *p);
+Point *copyPoint(Point *p);
+int isEqualPoints(Point *lp, Point *rp);
+
+#endif
